4cf27098b9e8ed78bde7e49785cad0c008a5b970
Stephen Johnson <stephen.c.johnson@gmail.com>
2022-12-01 10:11:13 -0500

    magical goodness

--- solution_01.py
+++ solution_01.py
@@ -1,3 +1,5 @@
+import aocd
+
 def max_cal(elves, num=1):
     cals = [sum(elf) for elf in elves]
     cals.sort(reverse=True)
@@ -6,10 +8,9 @@ def max_cal(elves, num=1):
 
 if __name__ == "__main__":
 
-    with open("input_01.txt", "r") as file:
-        input = file.read().split("\n\n")
+    input = aocd.get_data(day=1, year=2022)
 
-        elves = [[int(x) for x in sub.split("\n")] for sub in input]
+    elves = [[int(x) for x in sub.split("\n")] for sub in input.split("\n\n")]
 
-        print(max_cal(elves))
-        print(max_cal(elves, 3))
+    print(max_cal(elves))
+    print(max_cal(elves, 3))
