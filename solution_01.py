cf17ce750f430de891cb3432a3256bd1f54fa603
Stephen Johnson <stephen.c.johnson@gmail.com>
2022-12-01 10:28:08 -0500

    poor jen

--- solution_01.py
+++ solution_01.py
@@ -1,16 +1,13 @@
 import aocd
 
-def max_cal(elves, num=1):
-    cals = [sum(elf) for elf in elves]
-    cals.sort(reverse=True)
-    return sum(cals[0:num])
-
 
 if __name__ == "__main__":
 
     input = aocd.get_data(day=1, year=2022)
 
     elves = [[int(x) for x in sub.split("\n")] for sub in input.split("\n\n")]
+    cals = [sum(elf) for elf in elves]
+    cals.sort(reverse=True)
+    print(cals[0])
+    print(sum(cals[0:3]))
 
-    print(max_cal(elves))
-    print(max_cal(elves, 3))
