06e90ce84f558baccc4e569d95b5f97830324d8e
Stephen Johnson <stephen.c.johnson@gmail.com>
2022-12-04 15:58:10 -0500

    cleanup and insight

--- solution_03.py
+++ solution_03.py
@@ -1,5 +1,5 @@
 import aocd
-
+import itertools
 
 def calc_score(letter):
     if letter.islower():
@@ -9,32 +9,23 @@ def calc_score(letter):
 
 
 def doubled(rucksack):
-    half = int(len(rucksack) / 2)
-    for i in rucksack[0 : half + 1]:
-        if i in rucksack[half:]:
-            return i
-    return None
+    half = len(rucksack) // 2
+    return list(set.intersection(set(rucksack[0:half]), set(rucksack[half:])))[0]
 
 
 def tripled(rs1, rs2, rs3):
-    for i in rs1:
-        if i in rs2 and i in rs3:
-            return i
-    return None
+    return list(set.intersection(set(rs1), set(rs2), set(rs3)))[0]
 
 
 if __name__ == "__main__":
 
     input = aocd.get_data(day=3, year=2022)
 
-    score = 0
-    for rs in input.split("\n"):
-        score += calc_score(doubled(rs))
-
-    print(score)
+    scores = [calc_score(doubled(rs)) for rs in input.split("\n")]
+    print(sum(scores))
 
     score = 0
     rss = input.split("\n")
-    for i in range(int(len(rss) / 3)):
+    for i in range(len(rss) // 3):
         score += calc_score(tripled(rss[i * 3], rss[i * 3 + 1], rss[i * 3 + 2]))
     print(score)
